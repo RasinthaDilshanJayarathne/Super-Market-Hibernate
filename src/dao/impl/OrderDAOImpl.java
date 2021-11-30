31604203c81f2375f399d91436e7eff206252760
Radintha Dilshan <rasinthadilshan9@gmail.com>
2021-11-30 13:05:11 +0530

    'Update OrderDAOImpl.java'

--- src/dao/impl/OrderDAOImpl.java
+++ src/dao/impl/OrderDAOImpl.java
@@ -104,13 +104,34 @@ public class OrderDAOImpl implements OrderDAO {
 
     @Override
     public boolean ifOrderExist(String oid) throws SQLException, ClassNotFoundException {
-        ResultSet rst = CrudUtil.executeQuery("SELECT OrderId FROM Orders WHERE OrderId=?", oid);
-        return rst.next();
+        /*ResultSet rst = CrudUtil.executeQuery("SELECT OrderId FROM Orders WHERE OrderId=?", oid);
+        return rst.next();*/
+        Session session = FactoryConfigeration.getInstance().getSession();
+        Transaction transaction = session.beginTransaction();
+        Query query = session.createQuery("SELECT orderId FROM Order WHERE orderId=:oid");
+        query.setParameter("oid",oid);
+        transaction.commit();
+        session.close();
+        return true;
     }
 
     @Override
     public String generateNewOrderId() throws SQLException, ClassNotFoundException {
-        ResultSet rst = CrudUtil.executeQuery("SELECT OrderId FROM Orders ORDER BY OrderId DESC LIMIT 1;");
-        return rst.next() ? String.format("OD%03d", (Integer.parseInt(rst.getString("OrderId").replace("OD", "")) + 1)) : "OD001";
+        /*ResultSet rst = CrudUtil.executeQuery("SELECT OrderId FROM Orders ORDER BY OrderId DESC LIMIT 1;");
+        return rst.next() ? String.format("OD%03d", (Integer.parseInt(rst.getString("OrderId").replace("OD", "")) + 1)) : "OD001";*/
+
+        Session session = FactoryConfigeration.getInstance().getSession();
+        Transaction transaction = session.beginTransaction();
+        Query query = session.createSQLQuery("SELECT orderId FROM Order ORDER BY orderId DESC LIMIT 1");
+        String s = (String) query.uniqueResult();
+        if (s!=null) {
+            int orderId = Integer.parseInt(s.replace("O", "")) + 1;
+            s = String.format("O%03d", orderId);
+        } else {
+            return "O001";
+        }
+        transaction.commit();
+        session.close();
+        return s;
     }
 }
