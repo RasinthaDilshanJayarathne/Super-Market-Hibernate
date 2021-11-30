81926aaf188619c595318ad95bcdcfc108d88842
Radintha Dilshan <rasinthadilshan9@gmail.com>
2021-11-30 10:13:01 +0530

    'Add FactoryConfigeration'

--- src/util/validation/FactoryConfigeration.java
+++ src/util/validation/FactoryConfigeration.java
@@ -1,4 +1,25 @@
 package util.validation;
 
+import entity.*;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 public class FactoryConfigeration {
+    private static FactoryConfigeration factoryConfigeration;
+    private SessionFactory sessionFactory;
+
+    private FactoryConfigeration(){
+        Configuration configuration = new Configuration().configure().addAnnotatedClass(Customer.class).addAnnotatedClass(Item.class)
+                .addAnnotatedClass(Order.class).addAnnotatedClass(OrderDetail.class).addAnnotatedClass(Login.class);
+        sessionFactory=configuration.buildSessionFactory();
+
+    }
+    public static FactoryConfigeration getInstance(){
+        return (factoryConfigeration==null)? factoryConfigeration=new FactoryConfigeration() : factoryConfigeration;
+    }
+
+    public Session getSession(){
+        return sessionFactory.openSession();
+    }
 }
